8bcec1044fd7bf8ea91e63c7ae406bd7f735083c
AanandBhandari <aanandbhandari143@gmail.com>
2019-03-22 14:22:03 +0545

    inital commit

--- js/game.js
+++ js/game.js
@@ -1,151 +1,75 @@
-// create a new scene named "Game"
 let gameScene = new Phaser.Scene('Game');
-
-// game parameters
-// gameScene.playerSpeed = 1.5;
-// gameScene.enemySpeed = 2;
-// gameScene.enemyMaxY = 280;
-// gameScene.enemyMinY = 80;
-// gameScene.isPlayerAlive = true;
-
-// some parameters for our scene
-gameScene.init = function() {
+gameScene.init = function () {
   this.playerSpeed = 1.5;
   this.enemySpeed = 2;
-  this.enemyMaxY = 280;
-  this.enemyMinY = 80;
+  this.enemyMaxY =280;
+  this.enemyMinY =80;
+  this.enemiesArr = [];
+  this.numEnemies = 0;
 }
 
-// load asset files for our game
-gameScene.preload = function() {
+gameScene.preload = function () {
+  this.load.image('background','assets/background.png')
+  this.load.image('dragon', 'assets/dragon.png')
+  this.load.image('player', 'assets/player.png')
+  this.load.image('treasure', 'assets/treasure.png')
+}
+gameScene.create = function () {
+  let bg = this.add.sprite(0,0,'background')
+  // change the origin point to the left-top of the sprite
+  bg.setOrigin(0)
+  // console.log(this.sys.game);
+  this.player = this.add.sprite(40,this.sys.game.config.height/2,'player')
+  this.player.setScale(0.5)
 
-  // load images
-  this.load.image('background', 'assets/background.png');
-  this.load.image('player', 'assets/player.png');
-  this.load.image('dragon', 'assets/dragon.png');
-  this.load.image('treasure', 'assets/treasure.png');
-};
+  this.treasure = this.add.sprite(this.sys.game.config.width-80, this.sys.game.config.height / 2, 'treasure')
 
-// executed once, after assets were loaded
-gameScene.create = function() {
-
-  // background
-  let bg = this.add.sprite(0, 0, 'background');
-
-  // change origin to the top-left of the sprite
-  bg.setOrigin(0, 0);
-
-  // player
-  this.player = this.add.sprite(40, this.sys.game.config.height / 2, 'player');
-
-  // scale down
-  this.player.setScale(0.5);
-
-  // goal
-  this.treasure = this.add.sprite(this.sys.game.config.width - 80, this.sys.game.config.height / 2, 'treasure');
-  this.treasure.setScale(0.6);
-
-  // group of enemies
   this.enemies = this.add.group({
-    key: 'dragon',
-    repeat: 5,
-    setXY: {
-      x: 110,
-      y: 100,
-      stepX: 80,
-      stepY: 20
+    key : 'dragon',
+    repeat : 5,
+    setXY : {
+      x:110,
+      y:100,
+      stepX:80,
+      stepY:20,
     }
-  });
-
-  // scale enemies
-  Phaser.Actions.ScaleXY(this.enemies.getChildren(), -0.5, -0.5);
-
-  // set speeds
-  Phaser.Actions.Call(this.enemies.getChildren(), function(enemy) {
-    enemy.speed = Math.random() * 2 + 1;
-  }, this);
-
-  // player is alive
-  this.isPlayerAlive = true;
-};
-
-// executed on every frame (60 times per second)
-gameScene.update = function() {
-
-  // only if the player is alive
-  if (!this.isPlayerAlive) {
-    return;
-  }
-
-  // check for active input
+  })
+  Phaser.Actions.ScaleXY(this.enemies.getChildren(),-0.5,-0.5);
+  this.enemiesArr = this.enemies.getChildren();
+  this.numEnemies = this.enemiesArr.length;
+  Phaser.Actions.Call(this.enemiesArr,function (enemy) {
+    enemy.speed = Math.random()*2+1;
+  },this)
+}
+gameScene.update = function () {
   if (this.input.activePointer.isDown) {
-
-    // player walks
     this.player.x += this.playerSpeed;
   }
-
-  // treasure collision
-  if (Phaser.Geom.Intersects.RectangleToRectangle(this.player.getBounds(), this.treasure.getBounds())) {
+  if (Phaser.Geom.Intersects.RectangleToRectangle(this.player.getBounds(),this.treasure.getBounds() )) {
     this.gameOver();
   }
-
-  // enemy movement and collision
-  let enemies = this.enemies.getChildren();
-  let numEnemies = enemies.length;
-
-  for (let i = 0; i < numEnemies; i++) {
-
-    // move enemies
-    enemies[i].y += enemies[i].speed;
-
-    // reverse movement if reached the edges
-    if (enemies[i].y >= this.enemyMaxY && enemies[i].speed > 0) {
-      enemies[i].speed *= -1;
-    } else if (enemies[i].y <= this.enemyMinY && enemies[i].speed < 0) {
-      enemies[i].speed *= -1;
+  // console.log(this.numEnemies);
+  for (let i = 0; i <this.numEnemies; i++) {
+    console.log('hello');
+    this.enemiesArr[i].y +=this.enemiesArr[i].speed;
+    if (this.enemiesArr[i].y >= this.enemyMaxY && this.enemiesArr[i].speed > 0) {
+      this.enemiesArr[i].speed *= -1;
     }
-
-    // enemy collision
-    if (Phaser.Geom.Intersects.RectangleToRectangle(this.player.getBounds(), enemies[i].getBounds())) {
-      this.gameOver();
-      break;
+    else if (this.enemiesArr[i].y <= this.enemyMinY && this.enemiesArr[i].speed < 0) {
+      this.enemiesArr[i].speed *= -1;
     }
+    
   }
-};
 
-gameScene.gameOver = function() {
+}
+gameScene.gameOver = function () {
+  this.scene.restart()
+}
 
-  // flag to set player is dead
-  this.isPlayerAlive = false;
-
-  // shake the camera
-  this.cameras.main.shake(500);
-
-  // fade camera
-  this.time.delayedCall(250, function() {
-    this.cameras.main.fade(250);
-  }, [], this);
-
-  // restart game
-  this.time.delayedCall(500, function() {
-    this.scene.manager.bootScene(this);
-  }, [], this);
-
-  // reset camera effects
-  this.time.delayedCall(600, function() {
-    this.cameras.main.resetFX();
-  }, [], this);
-};
-
-
-
-// our game's configuration
 let config = {
-  type: Phaser.AUTO,
-  width: 640,
-  height: 360,
-  scene: gameScene
-};
-
-// create the game, and pass it the configuration
-let game = new Phaser.Game(config);
+  type : Phaser.AUTO,
+  width : 640,
+  height : 360,
+  scene : gameScene,
+}
+let game = new Phaser.Game(config)
